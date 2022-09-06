b35f10a881ff1253905912adde9458ce8d9b90eb
hzy <hzy9738@126.com>
2022-09-06 20:28:15 +0800

    button-component

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,15 @@
 import React from 'react';
+import Button, {ButtonSize, ButtonType} from "./components/Button/button";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Hello world</h1>
-        <h2>Hello world</h2>
-        <h3>Hello world</h3>
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Button>Hello World</Button>
+        <Button autoFocus btnType={ButtonType.Primary} size={ButtonSize.Large}>Hello World</Button>
+        <Button btnType={ButtonType.Primary} disabled>Disabled Button</Button>
+        <Button btnType={ButtonType.Link} href="http://www.baidu.com" target="_blank">baidu link</Button>
+        <Button btnType={ButtonType.Link} disabled href="http://www.baidu.com" target="_blank">baidu link</Button>
       </header>
     </div>
   );
